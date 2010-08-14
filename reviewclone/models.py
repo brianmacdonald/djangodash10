76cb46b33f9cc07d090767f6e0e1c344b85e22de
Brian Macdonald <brian.m17@gmail.com>
2010-08-14 01:24:07 -0400

    Inital models.

---
+++ reviewclone/models.py
@@ -0,0 +1,20 @@
+from django.conf import settings
+from django.db import models
+from django.contrib.auth.models import User
+
+class Item(models.Model):
+    name = models.CharField(max_length=155)
+    
+
+class Unit(models.Model):
+    user = models.ForeignKey(User)
+    item =models.ForeignKey(Item)
+    amount = models.FloatField()
+    
+
+class Relation(models.Model):
+    user_1 = models.ForeignKey(User, related_name='user 1') # Follower
+    user_2 = models.ForeignKey(User, related_name='user 2') # Followie
+    type = models.CharField(max_length=155, default='f')
+    is_active = models.BooleanField(default=True)
+ 
