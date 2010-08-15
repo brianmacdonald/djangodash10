6b9553afce1d47032b7518855a163a22f1632aaa
Brian Macdonald <brian.m17@gmail.com>
2010-08-14 21:20:22 -0400

    Fixed typo

--- example/forms.py
+++ example/forms.py
@@ -1,11 +1,11 @@
 from django import forms
 
-from django.contib.auth.models import User
+from django.contrib.auth.models import User
 
 class UserForm(forms.ModelForm):
     class Meta:
         model = User
-        fields(
+        fields = (
             'username',
             'first_name',
             'last_name',
